e344c9bf02bb99474db39ade1b8f6e4a9c99576f
abalarin <abalarin@outlook.com>
2019-04-20 23:06:47 -0400

    getting started

--- lintersect/models/build-engine.sh
+++ lintersect/models/build-engine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-snips-nlu generate-dataset en config.yaml entities/*.yaml intents/*.yaml  > dataset.json ; rm -rf engine; time snips-nlu train dataset.json engine;
+snips-nlu generate-dataset en lintersect/models/config.yaml lintersect/models/entities/*.yaml lintersect/models/intents/*.yaml  > lintersect/models/dataset.json ; rm -rf engine; time snips-nlu train lintersect/models/dataset.json lintersect/models/engine;
